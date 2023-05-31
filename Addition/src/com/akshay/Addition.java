abcaf1871e382359a3aa89e283d39b5ae9f3747a
Akshaybn99 <akshaybn888@gmail.com>
2023-05-31 14:58:02 +0530

    commit completed

---
+++ Addition/src/com/akshay/Addition.java
@@ -0,0 +1,13 @@
+package com.akshay;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		int num1=10;
+		int num2=30;
+		int num3=50;
+		System.out.println("Addition of two numbers");
+
+	}
+
+}
